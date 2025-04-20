d1cd6dd1b83f02f0dcaa8d3f7fac83f7b2c8cc24
kot149 <131101078+kot149@users.noreply.github.com>
2025-04-20 15:52:24 +0900

    Use connected_devices_with_services

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,10 @@ async fn async_main() -> Result<(), Box<dyn Error>> {
     adapter.wait_available().await?;
 
     info!("getting connected devices");
-    let devices = adapter.connected_devices().await?;
+    // let devices = adapter.connected_devices().await?;
+    let devices = adapter.connected_devices_with_services(&[BATTERY_SERVICE_UUID, BATTERY_LEVEL_UUID]).await?;
     for device in devices {
+        info!("found device: {:?}", device);
         let name = device.name()?.to_string();
         if name.contains("roBa") || name.contains("moNa2") {
             info!("target device found: {}", name);
